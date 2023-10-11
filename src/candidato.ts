082a9e9e00b7d998661c563beba9617d9916ab10
MarioGuisado <granadamariogg@gmail.com>
2023-10-11 18:27:25 +0200

    Idiomas y lenguajes de programacion añadidos
    Se ha creado la estructura que almacenará los lenguajes de programación y los idiomas del candidato closes #9

--- src/candidato.ts
+++ src/candidato.ts
@@ -5,6 +5,9 @@
     private correoElectronico: string;
     private telefono: string;
 
+    private idiomas: { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo"};
+    private lenguajesDeProgramacion: { [lenguaje: string]: "principiante" | "intermedio" | "avanzado" };
+
     getNombre(): string {
       return this.nombre;
     }
@@ -20,5 +23,13 @@
     getTelefono(): string {
       return this.telefono;
     }
-    
+
+    getIdiomas(): { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo" } {
+      return this.idiomas;
+    }
+  
+    getLenguajesDeProgramacion(): { [lenguaje: string]: "principiante" | "intermedio" | "avanzado" } {
+      return this.lenguajesDeProgramacion;
+    }
+
   }
