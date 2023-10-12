623f6d8fb74e552ae0851557f1e2b1bd870be0ce
MarioGuisado <granadamariogg@gmail.com>
2023-10-12 18:12:26 +0200

    Disponibilidad del candidato definida
    Se ha definido el rango de disponibilidad del candidato y la estructura que almacenará dicho dato, así como su getter correspondiente closes #10

--- src/candidato.ts
+++ src/candidato.ts
@@ -4,6 +4,7 @@
     private apellidos: string;
     private correoElectronico: string;
     private telefono: string;
+    private disponibilidad: "baja" | "media" | "alta";
 
     private idiomas: { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo"};
     private lenguajesDeProgramacion: { [lenguaje: string]: "principiante" | "intermedio" | "avanzado" };
@@ -22,6 +23,10 @@
   
     getTelefono(): string {
       return this.telefono;
+    }
+
+    getDisponibilidad(): "baja" | "media" | "alta" {
+      return this.disponibilidad;
     }
 
     getIdiomas(): { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo" } {
