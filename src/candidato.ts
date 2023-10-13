ad7bb27e50325ebb361975877eea4edba51c6658
MarioGuisado <granadamariogg@gmail.com>
2023-10-13 18:29:44 +0200

    Constructor de la clase Candidato
    Se ha definido el constructor de la clase Candidato closes #12

--- src/candidato.ts
+++ src/candidato.ts
@@ -9,6 +9,24 @@
     private idiomas: { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo"};
     private lenguajesDeProgramacion: { [lenguaje: string]: "principiante" | "intermedio" | "avanzado" };
 
+    constructor(
+      nombre: string,
+      apellidos: string,
+      correoElectronico: string,
+      telefono: string,
+      disponibilidad: "baja" | "media" | "alta",
+      idiomas: { [idioma: string]: "B1" | "B2" | "C1" | "C2" | "Nativo" },
+      lenguajesDeProgramacion: { [lenguaje: string]: "principiante" | "intermedio" | "avanzado" }
+    ) {
+      this.nombre = nombre;
+      this.apellidos = apellidos;
+      this.correoElectronico = correoElectronico;
+      this.telefono = telefono;
+      this.disponibilidad = disponibilidad;
+      this.idiomas = idiomas;
+      this.lenguajesDeProgramacion = lenguajesDeProgramacion;
+    }
+
     getNombre(): string {
       return this.nombre;
     }
