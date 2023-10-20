d0d109c54cdd4716873c02c70984bee7e0766506
MarioGuisado <granadamariogg@gmail.com>
2023-10-20 14:04:02 +0200

    Descripción eliminada soluciona https://github.com/manuelvico0102/easySelect/pull/7#discussion_r1366862492

--- src/oferta.ts
+++ src/oferta.ts
@@ -2,12 +2,10 @@ import { Candidato } from "./candidato";
 
 class Oferta {
     private id: string;
-    private descripcion: string;
     private candidatos: Candidato[];
 
-    constructor(id: string, descripcion: string, candidatos: Candidato[] = []) {
+    constructor(id: string, candidatos: Candidato[] = []) {
         this.id = id;
-        this.descripcion = descripcion;
         this.candidatos = candidatos;
     }
     
