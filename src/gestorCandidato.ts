cacd680a4c76d97085268ad8afd49ae63a7b1081
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-11-06 12:48:02 +0100

    Logica de negocio closes #23
    Se ha implementado la lógica de negocio, para la extraer la información de un candidato a través de un archivo preprocesado

--- src/gestorCandidato.ts
+++ src/gestorCandidato.ts
@@ -1,10 +1,49 @@
+import { Candidato } from "./candidato";
 
-class GestorCandidato {
+const fs = require('fs');
+
+export class GestorCandidato {
     archivo: string;
     datos: { [clave: string]: (archivo: string) => string } = {};
 
     constructor(filePath: string) {
         this.archivo = filePath;
     }
+
+    extraerCandidato(): Candidato {
+        const data = fs.readFileSync(this.archivo, 'utf8');
+        const lines = data.split('\n');
+        const candidatoData: { [key: string]: string } = {};
+
+        lines.forEach((line: string) => {
+            const [key, value] = line.split(': ');
+            if (value !== undefined) {
+                candidatoData[key] = value.trim();
+            }
+        });
+
+        const nombre = candidatoData['nombre'] || '';
+        const apellidos = candidatoData['apellidos'] || '';
+        const correoElectronico = candidatoData['correoElectronico'] || '';
+        const telefono = candidatoData['telefono'] || '';
+        const disponibilidad = candidatoData['disponibilidad'] as "baja" | "media" | "alta" || "";
+
+        const idiomasData = (candidatoData['idiomas'] || '').split(', ');
+        const idiomas: { [key: string]: "B1" | "B2" | "C1" | "C2" | "Nativo" } = {};
+        idiomasData.forEach((idiomaInfo: string) => {
+            const [idioma, nivel] = idiomaInfo.split(':');
+            idiomas[idioma] = nivel as "B1" | "B2" | "C1" | "C2" | "Nativo";
+        });
+
+        const lenguajesData = (candidatoData['lenguajesDeProgramacion'] || '').split(', ');
+        const lenguajesDeProgramacion: { [key: string]: "principiante" | "intermedio" | "avanzado" } = {};
+        lenguajesData.forEach((lenguajeInfo: string) => {
+            const [lenguaje, nivel] = lenguajeInfo.split(':');
+            lenguajesDeProgramacion[lenguaje] = nivel as "principiante" | "intermedio" | "avanzado";
+        });
+
+        return new Candidato(nombre, apellidos, correoElectronico, telefono, disponibilidad, idiomas, lenguajesDeProgramacion);
+    }
+
 }
 
