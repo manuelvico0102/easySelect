781fc6f4f6ceec1c1c6c56f3ad345ddf8e5b2ef0
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-11-11 10:44:47 +0100

    Modificado método para extraer datos #23
    Al cambiar el tipo de entrada de datos por los curriculum normalizados europeos en txt se ha tenido que rehacer el método, ahora se utiliza expresiones regulares más concretas para extraer los datos

--- src/gestorCandidato.ts
+++ src/gestorCandidato.ts
@@ -1,4 +1,5 @@
 import { Candidato } from "./candidato";
+import { Idiomas, LenguajesDeProgramacion, NivelIdioma, NivelLenguajeProgramacion } from "./types";
 import * as fs from 'fs';
 
 export class GestorCandidato {
@@ -11,37 +12,82 @@ export class GestorCandidato {
 
     extraerCandidato(): Candidato {
         const data = fs.readFileSync(this.archivo, 'utf8');
-        const lines = data.split('\n');
-        const candidatoData: { [key: string]: string } = {};
 
-        lines.forEach((line: string) => {
-            const [key, value] = line.split(': ');
-            if (value !== undefined) {
-                candidatoData[key] = value.trim();
-            }
-        });
+        const nombreCompletoMatch = data.match(/Nombre\n\n(.*)/);
+        const nombreCompleto = nombreCompletoMatch ? nombreCompletoMatch[1].trim() : '';
+        const [, apellidos, nombre] = nombreCompleto.match(/(.+), (.+)/) || [, '', ''];
+        
+        const correoElectronicoMatch = data.match(/Correo electrónico\n\n(.+)/);
+        const correoElectronico = correoElectronicoMatch ? correoElectronicoMatch[1].trim() : '';
+    
+        const telefonoMatch = data.match(/Teléfono\n\n(.+)/);
+        const telefono = telefonoMatch ? telefonoMatch[1].trim() : '';
+        
+        const idiomas = this.extraerIdiomas(data);
 
-        const nombre = candidatoData['nombre'] || '';
-        const apellidos = candidatoData['apellidos'] || '';
-        const correoElectronico = candidatoData['correoElectronico'] || '';
-        const telefono = candidatoData['telefono'] || '';
-
-        const idiomasData = (candidatoData['idiomas'] || '').split(', ');
-        const idiomas: { [key: string]: "B1" | "B2" | "C1" | "C2" | "Nativo" } = {};
-        idiomasData.forEach((idiomaInfo: string) => {
-            const [idioma, nivel] = idiomaInfo.split(':');
-            idiomas[idioma] = nivel as "B1" | "B2" | "C1" | "C2" | "Nativo";
-        });
-
-        const lenguajesData = (candidatoData['lenguajesDeProgramacion'] || '').split(', ');
-        const lenguajesDeProgramacion: { [key: string]: "principiante" | "intermedio" | "avanzado" } = {};
-        lenguajesData.forEach((lenguajeInfo: string) => {
-            const [lenguaje, nivel] = lenguajeInfo.split(':');
-            lenguajesDeProgramacion[lenguaje] = nivel as "principiante" | "intermedio" | "avanzado";
-        });
+        const lenguajesDeProgramacion = this.extraerLenguajesDeProgramacion(data);
 
         return new Candidato(nombre, apellidos, correoElectronico, telefono, idiomas, lenguajesDeProgramacion);
     }
 
+    private extraerIdiomas(data: string): Idiomas{
+        const idiomas: { [idioma: string]: { lectura: string, escritura: string, oral: string } } = {};
+        let lenguajeMaterno = '';
+
+        const regexIdioma = /otros idiomas\n+([^]*?)\n+Capacidades y aptitudes sociales/gi;
+        const regexDetalleIdioma = /([A-Za-záéíóúüñ\s]+)\s*•\s*Lectura\s*\n\n(\w+)\s*•\s*Escritura\s*\n\n(\w+)\s*•\s*Expresión oral\s*\n\n(\w+)/g;
+        const regexLenguaMaterna = /Lengua materna\s*\n\n\s*([A-Za-záéíóúüñ\s]+)\s*\n\notros idiomas/;
+
+        const matchLenguaMaterna = regexLenguaMaterna.exec(data);
+        if (matchLenguaMaterna) {
+            lenguajeMaterno = matchLenguaMaterna[1].trim();
+        }
+
+        const matchIdiomas = regexIdioma.exec(data);
+        if (matchIdiomas) {
+            const idiomasTexto = matchIdiomas[1];
+            let matchDetalle;
+            while ((matchDetalle = regexDetalleIdioma.exec(idiomasTexto)) !== null) {
+                const idioma = matchDetalle[1].trim();
+
+                if (idioma !== lenguajeMaterno) {
+                    const lectura = matchDetalle[2].trim();
+                    const escritura = matchDetalle[3].trim();
+                    const oral = matchDetalle[4].trim();
+
+                    idiomas[idioma] = { lectura, escritura, oral };
+                }
+            }
+        }
+        
+        const idiomasNormalizados: Idiomas = {};
+        if(lenguajeMaterno !== '')
+            idiomasNormalizados[lenguajeMaterno] = "Nativo" as NivelIdioma;
+        for (const idioma in idiomas) {
+            const { lectura, escritura, oral } = idiomas[idioma];
+            idiomasNormalizados[idioma] = this.elegirNivelIdioma(lectura, escritura, oral);
+        }
+        
+        return idiomasNormalizados;
+    }
+
+    private extraerLenguajesDeProgramacion(data: string): LenguajesDeProgramacion {
+        const regex = /([A-Za-z+]+): (\w+)/g;
+        const lenguajesDeProgramacion: LenguajesDeProgramacion = {};
+        
+        let match;
+        while ((match = regex.exec(data)) !== null) {
+            const lenguaje = match[1];
+            const nivelTexto = match[2].toLowerCase();
+            const nivel = nivelTexto as NivelLenguajeProgramacion;
+            lenguajesDeProgramacion[lenguaje] = nivel;
+        }
+        
+        return lenguajesDeProgramacion;
+    }
+    
+    
+    
+    
 }
 
