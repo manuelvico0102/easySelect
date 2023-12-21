5672109440f1e46c8d88626405b5d39e3081598d
manuelvico0102 <manuelvico0102@correo.ugr.es>
2023-12-21 09:53:06 +0100

    Se ha realizado la configuración closes #44
    Se ha configurado para que coja las variables del archivo .env, si no toma un valor por defecto las variables

---
+++ src/utils/configuracion.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export class Configuracion {
+    private logLevel: string;
+    private logFilePath: string;
+
+    constructor() {
+        this.logLevel = process.env.LOG_LEVEL || 'info';
+        this.logFilePath = process.env.LOG_FILE_PATH || './logs.log';
+    }
+
+    get LogLevel(): string {
+        return this.logLevel;
+    }
+
+    get LogFilePath(): string {
+        return this.logFilePath;
+    }
+}
